e6e68b2728db2fba58686a23de309392d1c0d486
Luceth Argote <97134341+bl8dyg1rl@users.noreply.github.com>
2025-02-23 22:53:00 -0500

    Update reconstruccion3D_pmap.py

--- reconstruccion3D_pmap.py
+++ reconstruccion3D_pmap.py
@@ -11,35 +11,33 @@ output_dir.mkdir(exist_ok=True)
 mvs_path = output_dir / "mvs"
 database_path = output_dir / "database.db"
 
-# Run the feature extraction
+#Extracción de caracteristicas
 pycolmap.extract_features(database_path, image_dir)
 
-# Run the exhaustive matcher
 pycolmap.match_exhaustive(database_path)
 
-# Run the incremental mapping
 maps = pycolmap.incremental_mapping(database_path, image_dir, output_dir)
 maps[0].write(output_dir)
 
-# Dense reconstruction without CUDA
+#Reconstrucción densa
 pycolmap.undistort_images(mvs_path, output_dir, image_dir)
-#pycolmap.patch_match_stereo(mvs_path, use_cuda=False)  # Disable CUDA
+#pycolmap.patch_match_stereo(mvs_path, use_cuda=False)  # Sin CUDA porque no tenga GPU NVIDIA
 pycolmap.stereo_fusion(mvs_path / "dense.ply", mvs_path)
 
-# Obtain the point cloud
+#Obtener la nube de puntos
 ply_path = mvs_path / "dense.ply"
 
 if not ply_path.exists():
     print('No se encontró el archivo PLY')
     exit()
 
-# Load the point cloud
+#Cargar la nube de puntos
 nube_puntos = o3d.io.read_point_cloud(str(ply_path))
 
-# Visualize the point cloud
+#Visualizar la nube de puntos
 o3d.visualization.draw_geometries([nube_puntos])
 
-# Example of setting SIFT options
+#SIFT
 sift_options = pycolmap.SiftExtractionOptions()
 sift_options.max_num_features = 512
 pycolmap.extract_features(database_path, image_dir, sift_options=sift_options)
